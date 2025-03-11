3a928390dc932e09e013996fb0a41ce6c4691f50
Corbin <htuan149@gmail.com>
2025-03-11 16:12:02 +0700

    feat: bai tap 1

--- tests/playwright-test/01-exercises/test1.spec.ts
+++ tests/playwright-test/01-exercises/test1.spec.ts
@@ -2,8 +2,9 @@ import {test} from '@playwright/test';
 
 const username = "Tuan Hoang";
 const email = "tuanhoang@gmail.com";
-const birthdate = "13/05/1997";
-const bio = "my test"
+const birthdate = "1997-05-13";
+const bio = "my test";
+const country = "Canada";
 
 
 test ("Exercies 1: Register Page", async ({page}) =>{
@@ -20,7 +21,15 @@ test ("Exercies 1: Register Page", async ({page}) =>{
         await page.locator("//input[@id='email']").fill(email);
         await page.locator("//input[@id='male']").click();
         await page.locator("//input[@id='traveling']").click();
-        
+        await page.locator("//option[@value='sports']").click();
+        await page.selectOption("//select[@id='country']",country);
+        await page.locator("//input[@id='dob']").fill(birthdate);
+        await page.locator("//textarea[@id='bio']").fill(bio);
+        await page.locator("//span[@class='slider round']").click();
+    })
+
+    await test.step("Click Register button", async() =>{
+        await page.locator("//button[@type='submit']").click();
     })
     
 })
