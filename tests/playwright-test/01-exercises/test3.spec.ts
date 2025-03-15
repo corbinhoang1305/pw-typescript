690023a4288fa9a6be84d2ea70ad9a2223a40a21
Corbin laptop <htuan149@gmail.com>
2025-03-15 20:58:40 +0700

    feat: complete test3

--- tests/playwright-test/01-exercises/test3.spec.ts
+++ tests/playwright-test/01-exercises/test3.spec.ts
@@ -10,6 +10,22 @@ test ("Exercies 3: Product page", async ({page}) =>{
     })
 
     await test.step("Them moi 100 todo item có noi dung 'Todo<i>'", async()=>{
-        
+        for (let i = 1 ; i<= 100; i++){
+            await page.locator("//input[@id='new-task']").fill(`Todo${i}`);
+            await page.locator("//button[@id='add-task']").click();
+        }
     })
+    // await page.waitForTimeout(10000); 
+    
+    await test.step("Xoa cac todo co so le", async()=>{
+        page.on("dialog", async dialog => {
+            await dialog.accept();
+        });
+        for (let i = 1 ; i<= 100; i++){
+            if (i % 2 !== 0){
+                await page.locator(`//button[@id='todo${i}-delete']`).click();
+            }
+        }
+    })
+    // await page.waitForTimeout(10000);
 })
